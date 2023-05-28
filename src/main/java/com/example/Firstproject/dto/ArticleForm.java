9417a359ccd47171c76173bb171b5c99bb1ddb56
YunJuHyung <wngud5289@naver.com>
2023-05-28 18:33:56 +0900

    update title content

--- src/main/java/com/example/Firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/Firstproject/dto/ArticleForm.java
@@ -11,9 +11,10 @@ import lombok.ToString;
     public class ArticleForm {
         private String title;
         private String content;
+        private Long id; //edit.mustache의 정보를 받아오는 id
 
         public Article toEntity(){
-            return new Article(null, title, content);
+            return new Article(id, title, content);
 
         }
 
